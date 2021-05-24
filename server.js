29ce7a8a9913bae46b1ac7cdb4f7cd099208ac8b
ismail hossain <ismailhsan45@gmail.com>
2021-05-24 16:34:49 +0600

    project building started

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = require('./router/router');
+const port = process.env.PORT || 3000;
+const hbs = require('express-handlebars');
+
+
+
+app.use(express.json());
+// serving static path
+app.use(express.static(path.join(__dirname, 'public')));
+
+// set view engine
+app.set('view engine', 'hbs');
+app.engine('hbs', hbs({
+    extname: 'hbs',
+    defaultView: 'default',
+    layoutsDir: path.join(__dirname, 'views'),
+    partialsDir: path.join(__dirname, 'views/partials')
+}));
+
+// routes
+app.use('/', router);
+
+// listening
+app.listen(port, () => console.log(`listening on http://localhost:${port}`))
